ba2da7f736d778f87edc1439ec4b086fe2768f4a
ceschoon <ceschoon@ulb.ac.be>
2021-02-13 15:03:33 +0100

    expectations

--- expectations/expectations.cpp
+++ expectations/expectations.cpp
@@ -11,20 +11,37 @@ using namespace std;
 
 Combination selectCombination_target(int columns[][4], int markers[][2], int player, vector<Combination> &combinations);
 
-int target;
+int target = -1;
 
 
 
 ////////////////////////////////////////////////////////////////////////////
 
 
-int main()
+int main(int argc, char **argv)
 {
-	///////////////////////////////
-	// Parameters and init
+	//////////////////////////////
+	// Options
+	
+	bool specific_target = false;
+	bool already_marked  = false;
+	int  columns_to_fill = 0; // staring from center
+	
+	if (argc>1) if (string(argv[1])=="true") specific_target = true;
+	if (argc>2) if (string(argv[2])=="true") already_marked  = true;
+	if (argc>3) columns_to_fill = stoi(argv[3]);
+	
+	cout << "Options:" << endl;
+	cout << "  specific_target = " << specific_target << endl;
+	cout << "  already_marked  = " << already_marked  << endl;
+	cout << "  columns_to_fill = " << columns_to_fill << endl;
+	cout << endl;
+	
+	
+	//////////////////////////////////////////
+	// Simulation parameters
 	
 	SimulParams sparam;
-	SimulState  sstate_in; initialiseDefaultState(sstate_in);
 	
 	sparam.singleplayer = true;
 	sparam.playOneTurn = true;
@@ -32,9 +49,11 @@ int main()
 	sparam.seed = 0; // will be overwritten
 	
 	decideToStop = &decideToStop_never;
-	selectCombination = &selectCombination_any;
+	if (specific_target) selectCombination = &selectCombination_target;
+	else                 selectCombination = &selectCombination_any;
 	
 	int numGames = 10000;
+	
 	
 	////////////////////////////////////////////////
 	// Rng for choosing seeds of each simulation
@@ -45,6 +64,7 @@ int main()
 	uniform_int_distribution<int> dist(0,1000000);
 	default_random_engine gen(seed);
 	
+	
 	////////////////////////////////
 	// Simulations and analysis
 	
@@ -52,6 +72,29 @@ int main()
 	
 	for (target=2; target<=12; target++)
 	{
+		// Initial simulation state (fill columns and position markers)
+		
+		SimulState sstate_in; initialiseDefaultState(sstate_in);
+		
+		int fill_order[11] = {7,6,8,5,9,4,10,3,11,2,12};
+		int columns_filled = 0;
+		
+		for (int i=0; i<11 && columns_filled<columns_to_fill; i++)
+		{
+			int n = fill_order[i];
+			if (n!=target)
+			{
+				sstate_in.columns[index(n)][3] = height(index(n))-1;
+				columns_filled++;
+			}
+		}
+		
+		if (already_marked)
+		{
+			sstate_in.markers[0][0] = index(target);
+			sstate_in.markers[0][1] = 0;
+		}
+		
 		// Runs games and record progression for marker in target column
 		// Note: there could be none, so init to zero
 		
@@ -59,11 +102,14 @@ int main()
 		for (int igame = 0; igame<numGames; igame++)
 		{
 			sparam.seed = dist(gen);
-			SimulState sstate_out = sstate_in; 
+			SimulState sstate_out = sstate_in;
 			simulGame(sparam, sstate_out);
 			
 			for (int i=0; i<3; i++) if (sstate_out.markers[i][0]==index(target))
-				progression[igame] = sstate_out.markers[i][1];
+			{
+				if (already_marked) progression[igame] =   sstate_out.markers[i][1];
+				else                progression[igame] = 1+sstate_out.markers[i][1];
+			}
 		}
 		
 		// Compute average progression in the target column
@@ -73,6 +119,7 @@ int main()
 			progression_avg[index(target)] += progression[igame];
 		progression_avg[index(target)] /= numGames;
 	}
+	
 	
 	///////////////////////////
 	// Print results
@@ -84,6 +131,7 @@ int main()
 		cout << setw(8) << progression_avg[index(7)]/progression_avg[index(target)];
 		cout << endl;
 	}
+	
 	
 	return 0;
 }
@@ -106,9 +154,9 @@ Combination selectCombination_target(int columns[][4], int markers[][2], int pla
 {
 	// First, search for combinations containing the target value
 	
-	vector<Combination> targetcomb; 
+	vector<Combination> targetcomb;
 	
-	for (int i=0; i<combinations.size(); i++) 
+	for (int i=0; i<combinations.size(); i++)
 	{
 		// If both values are the target value, select without hesitation
 		if (combinations[i].val1==target && combinations[i].val2==target)
@@ -147,29 +195,38 @@ Combination selectCombination_target(int columns[][4], int markers[][2], int pla
 	// If no combination contains the target value, search the combination
 	// with the *two* most likely values to get another time 
 	
-	else
+	if (targetcomb.size()==0)
 	{
 		// Find best combination
 		
 		int ibest = 0;
-		for (int i=0; i<targetcomb.size(); i++)
+		for (int i=0; i<combinations.size(); i++)
 		{
-			if (abs(targetcomb[i].val1-7)<abs(targetcomb[ibest].val1-7) && 
-			    abs(targetcomb[i].val2-7)<abs(targetcomb[ibest].val2-7)) 
+			if (abs(combinations[i].val1-7)<abs(combinations[ibest].val1-7) && 
+			    abs(combinations[i].val2-7)<abs(combinations[ibest].val2-7)) 
 				ibest = i;
 		}
 		
+		// If there is not combination with twice the same value,
+		// select this one to save a marker
+		// Note: not clear it this is best to do or not,
+		//       the data shows little difference with and without 
+		
+		for (int i=0; i<combinations.size(); i++)
+			if (combinations[i].val1 == combinations[i].val2) ibest = i;
+		
 		// Put the value closest to 7 in the first position
 		
-		struct Combination comb = targetcomb[ibest];
+		struct Combination comb = combinations[ibest];
 		if (abs(comb.val2-7)<abs(comb.val1-7))
 		{
-			comb.val1 = targetcomb[ibest].val2;
-			comb.val2 = targetcomb[ibest].val1;
+			comb.val1 = combinations[ibest].val2;
+			comb.val2 = combinations[ibest].val1;
 		}
 		
 		return comb;
 	}
+	
 	
 	// default return statement
 	return combinations[0];
